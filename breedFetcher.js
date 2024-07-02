5c3afa104cebd3058a0a16f0ea2bd359ace935fb
Henry <mbahenryemeka@gmail.com>
2024-07-02 15:48:03 -0400

    code refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const needle = require('needle');
-const breed = process.argv[2];
-const searchUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-needle.get(searchUrl,(err,res) =>{
-  if (err) {
-    console.log(`Failed to fetch breed: ${err}`);
-    return;
-  } else {
-    if (res.body.length > 0) {
-      console.log(res.body[0].description);
-    } else {
-      console.log(`breed not found`);
+const fetchBreedDescription = function(breedName, callback) {
+  const searchUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  needle.get(searchUrl,(err,res) =>{
+    if (err) {
+      return callback(err, null);
     }
-  }
-});
+    if (res.body.length > 0) {
+      callback(null, res.body[0].description);
+    } else {
+      callback('breed not found', null);
+    }
 
+  });
+};
+
+module.exports = {fetchBreedDescription};
